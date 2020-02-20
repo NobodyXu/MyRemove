0cf9a325805b51339b809370995af98166e606a3
nobodyxu <Jiahao_XU@outlook.com>
2020-02-20 17:38:24 +1100

    Catch OSError and print them
    so that no traceback is performed, as OSError does not mean bug
    and is used more like a warning to the user

--- myremove.py
+++ myremove.py
@@ -10,16 +10,26 @@ def rename(dirname, filename, newfilename):
     print(f"The file {filename} in {dirname} has been renamed to {newfilename}")
     os.rename(dirname + "/" + filename, dirname + "/" + newfilename)
 
-path = input("Please enter the directory name: ")
-word = input("Please enter the prefix/postfix that need to be removed: ")
+def main():
+    path = input("Please enter the directory name: ")
+    word = input("Please enter the prefix/postfix that need to be removed: ")
+    
+    for root, dirnames, filenames in os.walk(path, onerror = onerror):
+        for filename in filenames:
+            if filename.startswith(word):
+                newfilename = filename[len(word): ]
+            elif filename.endswith(word):
+                newfilename = filename[:-len(word)]
+            else:
+                continue
+    
+            rename(root, filename, newfilename)
 
-for root, dirnames, filenames in os.walk(path, onerror = onerror):
-    for filename in filenames:
-        if filename.startswith(word):
-            newfilename = filename[len(word): ]
-        elif filename.endswith(word):
-            newfilename = filename[:-len(word)]
-        else:
-            continue
+# Catch OSError and print them to avoid traceback
+# as OSError does not necessary mean bugs
+# and is used more like a warning to the user
+try:
+    main()
+except OSError as exception:
+    print(exception)
 
-        rename(root, filename, newfilename)
