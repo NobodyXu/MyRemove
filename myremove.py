20f1c67a2b24629d43660914f07675838fa31077
nobodyxu <Jiahao_XU@outlook.com>
2020-02-20 16:26:03 +1100

    Do not use forking as it is not necessary

--- myremove.py
+++ myremove.py
@@ -1,27 +1,15 @@
 import sys
 import os
 
-def forking(command,filename,stripword):
-    try:
-        pid = os.fork()
-    except:
-        sys.exit("ERROR! FORKING FAILS!")
-
-    if pid == 0:
-        #child
-        newfilename = filename.split('/')
-        print(newfilename[1])
-        prefix = newfilename[1].split('.')
-        newfilename[1] = prefix[0].strip(stripword) + '.' + prefix[1]
-        newfilename = newfilename[0] + '/' + newfilename[1]
-        print(f"The file {filename} has been renamed to {newfilename}")
-        os.rename(filename,newfilename)
-        sys.exit()
-        #normal exit
-    else:
-        exit_code = os.waitpid(pid,0)
-        if exit_code[1] != 0:
-            sys.exit("ERROR! EXITING WITH NON ZERO CODE!")
+def rename(command, filename, stripword):
+    #child
+    newfilename = filename.split('/')
+    print(newfilename[1])
+    prefix = newfilename[1].split('.')
+    newfilename[1] = prefix[0].strip(stripword) + '.' + prefix[1]
+    newfilename = newfilename[0] + '/' + newfilename[1]
+    print(f"The file {filename} has been renamed to {newfilename}")
+    os.rename(filename,newfilename)
 
     
 path = input("Please enter the directory name: ")
