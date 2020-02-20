1382aba749b9b1238d2cb2070c67afd03e2e42d4
nobodyxu <Jiahao_XU@outlook.com>
2020-02-20 16:44:05 +1100

    Fix bugs, remove unnecessary bloated code

--- myremove.py
+++ myremove.py
@@ -2,21 +2,16 @@
 
 import os
 
-def rename(filename, stripword):
-    newfilename = filename.split('/')
-    print(newfilename[1])
-    prefix = newfilename[1].split('.')
-    newfilename[1] = prefix[0].strip(stripword) + '.' + prefix[1]
-    newfilename = newfilename[0] + '/' + newfilename[1]
-    print(f"The file {filename} has been renamed to {newfilename}")
-    os.rename(filename,newfilename)
-
+def rename(dirname, filename, newfilename):
+    print(f"The file {filename} in {dirname} has been renamed to {newfilename}")
+    os.rename(dirname + "/" + filename, dirname + "/" + newfilename)
     
 path = input("Please enter the directory name: ")
 word = input("Please enter the prefix/postfix that need to be removed: ")
 
 for root, dirnames, filenames in os.walk(path):
     for filename in filenames:
-        if word in filename:
-            full_com = root + "/" + filename
-            rename(full_com, word)
+        if filename.startswith(word):
+            rename(root, filename, filename[len(word) :])
+        elif filename.endswith(word):
+            rename(root, filename, filename[:-len(word)])
